b5a4f7f281b557b585b520638fb3925f1c347a0b
Sebastiaan Speck <sebastiaan.speck@keronic.com>
2026-06-04 18:29:36 +0200

    Apply grammar fix

--- prism/prism.js
+++ prism/prism.js
@@ -12563,7 +12563,7 @@ Prism.languages.magik = {
 
 	'variable': [
 		/\|[\w?!]+\|/, // variable encased like |var|, |0|, |123|
-		{ pattern: /(^|[^.])\b[a-z]\w*\b/i, lookbehind: true },
+		{ pattern: /(^|[^.])\b[a-z][\w?!]*/i, lookbehind: true },
 	],
 };
 
